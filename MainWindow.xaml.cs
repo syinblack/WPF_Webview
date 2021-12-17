fb42d1df9da550a3273c490a62077635c794f79b
meditation.lee <meditation.lee@nhn.com>
2021-12-17 17:12:32 +0900

    WebBrowser 확장 메소드 추가

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Security.Permissions;
 using System.Runtime.InteropServices;
+using MeditationExtension;              // made by meditation
+using System.Collections.Generic;
 
 namespace WpfApp1
 {
@@ -13,21 +14,14 @@ namespace WpfApp1
         {
             InitializeComponent();
 
-            // 네이티브에 웹뷰를 연결하여 스크립트 사용
             _WebBrowser.ObjectForScripting = this;
-
-            // 초기 웹뷰 주소
-            Uri source = new Uri("https://www.naver.com");
-            //string source = @"file:///C:\Users\NHN\source\repos\WpfApp1\page1.html";
-            _WebBrowser.Navigate(@$"{source}");
-
+            _WebBrowser.Navigated += _WebBrowser_Navigated;
+            _WebBrowser.Navigate($"https://www.naver.com");
         }
 
-     
         // 네이티브(WPF) 환경에서 웹뷰(html)를 구현하고, 양방 통신 핸들링을 최종 목적으로 한다.
         // {xaml, C#(이하 cs)} 와 {html, JavaScript(이하 js)}간 양방향 통신으로 함수를 구현해보는 실습
         // 네이티브 C#은 메소드, JavaScipt는 함수로 구분한다.
-        
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             if (_WebBrowser.CanGoBack)
@@ -70,75 +64,32 @@ namespace WpfApp1
         // 웹뷰의 함수를 호출하여 네이티브의 인자를 전달한다.
         private void TransferDataToWebviewButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                int[] inputArray = new int[] {
-                Cal_ComboBox.SelectedIndex,
-                int.Parse(NativeTextBox1.Text),
-                int.Parse(NativeTextBox2.Text) };
+            object res = _WebBrowser.CalculateInputs(Cal_ComboBox.SelectedIndex, NativeTextBox1.Text, NativeTextBox2.Text);
 
-                // WebBrowser 컨트롤에 연결된 JavaScript 함수를 호출
-                object res = _WebBrowser.InvokeScript("Calculate", inputArray[0], inputArray[1], inputArray[2]);
-
-                NativeTextBox3.Text = res.ToString();
-            }
-            catch (COMException)
-            {
-                _ = MessageBox.Show("Html 스크립트를 먼저 호출하세요.");
-            }
-            catch (FormatException)
-            {
-                _ = MessageBox.Show("Native Input을 정수로 채워야 합니다.");
-            }
-            catch (OverflowException)
-            {
-                _ = MessageBox.Show("정수형 범위가 Int32를 초과합니다.");
-            }
+            NativeTextBox3.Text = res.ToString();
         }
 
         // 웹뷰에서 url 변경하기 전에 이벤트 핸들러 등록
+        // _WebBrowser_Navigating 이벤트 핸들러는 웹뷰에서 url 변경하기 전에만 추가되고, 나머지 경우에는 제거된 상태로 있어야 함.
         public void UrlChangingFromJavaScript()
         {
-            _WebBrowser.Navigating += Navigating_EventHandler; // url 파싱 이벤트 핸들러
+            _WebBrowser.Navigating += _WebBrowser_Navigating; // url 파싱 이벤트 핸들러
         }
 
-        void Navigating_EventHandler(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        // 웹뷰로부터 url 변경 신호를 받으면 (url 변경전에) 파싱한다.
+        // url scheme : {protocol}:{host}?{op=}?{param1=}?{param2=}
+        private void _WebBrowser_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
         {
-            string uri = e.Uri.ToString();
+            // ParseURL: url을 파싱하여 {protocol, host, cmd, param1, param2} 순으로 반환
+            List<string> URLResources = WebBrowserExtension.ParseURL(e.Uri.ToString());
 
-            string[] words = uri.Split("?");
-            string[] word = words[0].Split(":");
+            _WebBrowser.FillParsedResourceInControls(URLResources, Cal_ComboBox, NativeTextBox1, NativeTextBox2);
+        }
 
-            string protocol = word[0];
-            string host = word[1];
-
-            string cmd = words[1].Substring(3);     // op=add
-            string param1 = words[2].Substring(7);  // param1=1
-            string param2 = words[3].Substring(7);  // param2=2
-
-            // 새로 웹뷰를 호출하기 전에 이벤트 핸들러 제거. (이벤트 무한 루프 발생하지 )
-            _WebBrowser.Navigating -= Navigating_EventHandler;
-            switch (protocol)
-            {
-                case "calc":
-                    _WebBrowser.Navigate("file:///C:/Users/NHN/source/repos/WpfApp1/page1.html");
-
-                    if(cmd == "Add") { Cal_ComboBox.SelectedIndex = 0;}
-                    else if(cmd == "Sub") { Cal_ComboBox.SelectedIndex = 1;}
-                    else if(cmd == "Mul") { Cal_ComboBox.SelectedIndex = 2;}
-                    else if(cmd == "Div") { Cal_ComboBox.SelectedIndex = 3;}
-
-                    NativeTextBox1.Text = param1;
-                    NativeTextBox2.Text = param2;
-                    
-                    break;
-                case "http":
-                case "https":
-                    _WebBrowser.Navigate("https://" + host);
-                    break;
-                default:
-                    break;
-            }
+        // url에 의해 페이지가 로드된 후 _WebBrowser_Navigating가 등록되어 있다면 제거한다.
+        private void _WebBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            _WebBrowser.Navigating -= _WebBrowser_Navigating;
         }
     }
 }
